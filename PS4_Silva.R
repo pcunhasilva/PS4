12fb0d14dd570da1f31f4f7e545724cec518e7ed
pcunhasilva <patricksilva27@gmail.com>
2017-03-03 00:36:21 -0600

    Finish to clean data
    Clean data from Wiki (ex.1)

--- PS4_Silva.R
+++ PS4_Silva.R
@@ -2,11 +2,12 @@
 ## PS 5625 - Applied Statistical Programming
 ## Problem Set 4
 ## Author: Patrick Cunha Silva
-## Tasks: 1 - XXXXXX.
+## Tasks: 1 - Scrap Wiki and Clean the Data.
 
 # Clean session before starting
 rm(list = ls())
 library(rvest)
+library(dplyr)
 
 # Define the URL
 wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
@@ -18,32 +19,34 @@ temp <- wikiURL %>%
    html_table()
 
 # Select the table of interest
-table1 <- temp[[2]]
+pop_vote_margin <- temp[[2]]
+pop_vote_margin <- pop_vote_margin[-c(1,2),]
 
 # Add collunm names:
-colnames(table1)[1] <- "id"
-colnames(table1)[2] <- "election_year"
-colnames(table1)[3] <- "w_name"
-colnames(table1)[4] <- "w_party"
-colnames(table1)[5] <- "w_votes_elec_col"
-colnames(table1)[6] <- "w_votes_elec_col_perc"
-colnames(table1)[7] <- "w_pop_votes_perc"
-colnames(table1)[8] <- "w_pop_votes_margin_perc"
-colnames(table1)[9] <- "w_pop_votes"
-colnames(table1)[10] <- "w_pop_votes_margin"
-colnames(table1)[11] <- "l_name"
-colnames(table1)[12] <- "l_party"
-colnames(table1)[13] <- "turnout"
+colnames(pop_vote_margin) <- c("id",  "election_year", "w_name", "w_party",
+                      "w_votes_elec_col", "w_votes_elec_col_perc",
+                      "w_pop_votes_perc", "w_pop_votes_margin_perc",
+                      "w_pop_votes", "w_pop_votes_margin", "l_name",
+                      "l_party",  "turnout")
+    
+# For numeric variables:
+cleanNum <- function(x){
+   x <- gsub("\\[|a\\]", "", x)
+   x <- gsub("%", "", x)
+   x <- gsub("−", "-", x)
+   x <- gsub(",", "", x)
+   x <- as.numeric(x)
+}
 
-# Clean and recode the data
-## id
-table1[,1] <- as.numeric(table1[,1])
-## w_name
-table1[,3] <- gsub(",", ", ", table1[,3])
-## w_
+# For names (winner and loser) variables
+cleanNames <- function(x){
+   x <- gsub("\\[|a\\]", "", x)
+   x <- gsub(".*,", "", x)
+}
 
-#####
-table1 <- table1[-c(1,2),]
-table1[, 7] <- gsub("\\[|a\\]", "", table1[,7])
-table1[, 7] <- gsub("%", "", table1[,7])
+# Clean numeric variables
+pop_vote_margin[, c(1, 2, 6, 7, 8,9, 10, 13)] <- apply(pop_vote_margin[, c(1, 2, 6, 7, 8,9, 10, 13)], 2, 
+                                              cleanNum) 
+# Clean name variables
+pop_vote_margin[, c(3, 11)] <- apply(pop_vote_margin[, c(3, 11)], 2, cleanNames)
 
