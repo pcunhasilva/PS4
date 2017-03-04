1ff0f2e5ba5cac097b97a31bc5388d25b927bac1
pcunhasilva <patricksilva27@gmail.com>
2017-03-04 01:06:53 -0600

    Generate Plots
    Generate the plots.

--- PS4_Silva.R
+++ PS4_Silva.R
@@ -3,7 +3,8 @@
 ## Problem Set 4
 ## Author: Patrick Cunha Silva
 ## Tasks: 1 - Scrap First Wiki and Clean the Data.
-## Tasks: 2 - Scrap Second Wiki, Clean the Data, Merge and Save dataset.
+## Tasks: 2 - Scrap Second Wiki, Clean the Data, Merge and Save dataset in a .RData file.
+## Tasks: 3 - Generate the Plots and Save them in a .PDF file.
 
 # Clean session before starting
 rm(list = ls())
@@ -24,7 +25,7 @@ pop_vote_margin <- temp[[2]]
 pop_vote_margin <- pop_vote_margin[-c(1,2),]
 
 # Add collunm names:
-colnames(pop_vote_margin) <- c("id",  "election_year", "w_name", "w_party",
+colnames(pop_vote_margin) <- c("election_number",  "election_year", "w_name", "w_party",
                       "votes_elec_col", "w_votes_elec_col_perc",
                       "w_pop_votes_perc", "w_pop_votes_margin_perc",
                       "w_pop_votes", "w_pop_votes_margin", "rup_name",
@@ -111,7 +112,75 @@ complete_data <- merge(complete_data, temp, by = c("rup_name", "election_year"))
 names_var <- names(pop_vote_margin)
 complete_data <- complete_data[,c(names_var, "w_elec_col", "rup_elec_col")]
 
+# Sort data.frame
+order_vec <- order(complete_data[,1])
+complete_data <- complete_data[order_vec,]
+
 # Save data.frame in a .R file
+setwd("~/Google Drive/2. Academicos/6. Doutorado/Class/2017/Spring/PS 5625 - Applied Statistical Programming/Homeworks/Problem Sets/PS4")
 save(complete_data, file = "complete_data.RData")
 
+######################################################
+# Plots
+# Attach the dataset.
+attach(complete_data)
+# Define plot's name
+pdf("Trends_on_Popular_Vote_Margin.pdf")
 
+# Define plot's parameters and layout
+par(c(4, 3, 3, 1))
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+
+# Tendence of winner % Popular Vote and % Electoral College:
+plot(x = election_year, y = w_pop_votes_perc,
+     type = "p", pch = 19, cex = 0.4,
+     ylim = c(0, 100), axes = FALSE,
+     xlab = "Election Year",
+     ylab = "% of Votes",
+     main = "Percentage of Popular and \n Electoral College Votes")
+lines(x = election_year, y = w_pop_votes_perc, lty = 1)
+points(x = election_year, y = w_votes_elec_col_perc,
+      pch = 17, cex = 0.4)
+lines(x = election_year, y = w_votes_elec_col_perc, lty = 2)
+axis(side = 1, at = c(1824, 1856, 1888, 1920, 1952, 1984, 2016))
+axis(side = 2, at = c(0, 25, 50, 75, 100))
+legend("bottomright",
+       legend = c("Popular Vote", "Electoral College"),
+       lty = c(1,3),
+       cex = .8)
+
+# Is the Electoral College Vote Margin function of the Popular Vote Margin?
+plot(x = w_pop_votes_margin_perc, y = (w_elec_col-rup_elec_col),
+     xlim = c(min(w_pop_votes_margin_perc)*1.4, max(w_pop_votes_margin_perc)*1.2),
+     ylim = c(min((w_elec_col-rup_elec_col))*1.10, max((w_elec_col-rup_elec_col))*1.10),
+     axes = FALSE, pch = 19, cex = 0.5, 
+     xlab = "Popular Vote Margin",
+     ylab = "Electoral College Margin",
+     main = "Electoral College Margin as function \n of Popular Vote Margin")
+abline(reg = lm((w_elec_col-rup_elec_col)~w_pop_votes_margin_perc))
+axis(side = 1, at = round(seq(-15, 30, length.out = 5), 0))
+axis(side = 2, at = round(seq(-20, 570, length.out = 5), 0))
+
+# Does Popular Vote Margin Differ By Party?
+density_dem <- density(w_pop_votes_margin_perc[w_party=="Dem."])
+density_rep <- density(w_pop_votes_margin_perc[w_party=="Rep."])
+density_others <- density(w_pop_votes_margin_perc[w_party!="Rep." & w_party!="Dem."])
+hist(w_pop_votes_margin_perc, probability=TRUE, 
+     breaks = 15, xlim = c(-30, 40),
+     xlab = "Popular Vote Margin",
+     main = "Distribution of Popular Vote \n Margin by Party")
+lines(density_others, col = "yellow")
+lines(density_rep, col = "red")
+lines(density_dem, col = "blue")
+rug(w_pop_votes_margin_perc)
+legend("topright",
+       legend = c("Democratic", "Republican", "Others"),
+       col = c("blue", "red", "yellow"),
+       lty = c(1, 1, 1),
+       cex = .8)
+dev.off()
+
+# Detach dataset.
+detach(complete_data)
+
+# Explanation about the plots:
